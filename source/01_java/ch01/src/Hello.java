76da66ba66ef6a0a95e855fbb317c4e56d7e05d3
naradin711 <hajime711@naver.com>
2022-03-10 16:47:27 +0900

    first commit

---
+++ source/01_java/ch01/src/Hello.java
@@ -0,0 +1,12 @@
+/*
+ 2022. 3. 10. 첫번째 자바 프로그램. 류지환
+ */
+public class Hello {
+     public static void main(String [] args) {
+         System.out.println("Hello, Java");
+         System.out.print("Welcome~");
+         System.out.print("끝");
+         // 한 줄 주석 - 설명문 (컴퓨터는 실행하지 않는 실형문)
+         // 콘솔 출력 : println(개행포함), print(개행미포함)
+  }
+}
